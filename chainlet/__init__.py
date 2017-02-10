36376318d18c4cbeed8773c65558b5013e327afd
Max Fischer <maxfischer2781@gmail.com>
2017-02-10 17:36:36 +0100

    exporting names at package level

---
+++ chainlet/__init__.py
@@ -0,0 +1,6 @@
+from __future__ import absolute_import
+from .chainlink import ChainLink
+from .funclink import FunctionLink
+from .genlink import GeneratorLink
+
+__all__ = [ChainLink.__name__, FunctionLink.__name__, GeneratorLink.__name__]
