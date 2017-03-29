4c4d6ac57698f20520bdebccf499ab38afc1adb1
Max Fischer <maxfischer2781@gmail.com>
2017-03-29 11:54:51 +0200

    added asynchronous driver test

--- chainlet_unittests/chainlet/driver.py
+++ chainlet_unittests/chainlet/driver.py
@@ -2,6 +2,7 @@ from __future__ import absolute_import, division
 import unittest
 import itertools
 import random
+import time
 
 import chainlet.driver
 
@@ -16,18 +17,25 @@ class DriverMixin(object):
         driver = self.driver_class()
         results = []
         elements = [Adder(val) for val in (0, -2, 2, 1E6, random.randint(-256, 256), random.randint(-256, 256))]
-        for elements in itertools.product(elements, repeat=3):
-            with self.subTest(elements=elements):
-                initials = [0, 2, 1E6, -1232527]
-                expected = [initial + sum(element.value for element in elements) for initial in initials]
-                a, b, c = elements
-                buffer = Buffer()
-                chain = produce(initials) >> a >> b >> c >> buffer
-                driver.mount(chain)
-                results.append([expected, buffer])
-        driver.run()
-        for expected, buffer in results:
-            self.assertEqual(expected, buffer.buffer)
+        for run_async in (True, False):
+            with self.subTest(run_async=run_async):
+                for elements in itertools.product(elements, repeat=3):
+                    initials = [0, 2, 1E6, -1232527]
+                    expected = [initial + sum(element.value for element in elements) for initial in initials]
+                    a, b, c = elements
+                    buffer = Buffer()
+                    chain = produce(initials) >> a >> b >> c >> buffer
+                    driver.mount(chain)
+                    results.append([expected, buffer])
+                if run_async:
+                    driver.start()
+                    time.sleep(0.1)  # let the driver startup
+                    while driver.running:
+                        time.sleep(0.1)
+                else:
+                    driver.run()
+                for expected, buffer in results:
+                    self.assertEqual(expected, buffer.buffer)
 
 
 class TestChainDriver(DriverMixin, unittest.TestCase):
