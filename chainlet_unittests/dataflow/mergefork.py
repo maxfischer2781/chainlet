80dd4ec3305d40517932f10401786d9a33e3e5f6
Max Fischer <maxfischer2781@gmail.com>
2017-04-04 13:24:10 +0200

    added merge tests for custom types and empty sequences

--- chainlet_unittests/dataflow/mergefork.py
+++ chainlet_unittests/dataflow/mergefork.py
@@ -1,7 +1,7 @@
 import itertools
 import unittest
 
-import chainlet
+import chainlet.dataflow
 
 from ..utility import produce
 
@@ -15,6 +15,25 @@ def produce_iterables(iterable):
 
 
 class ChainMerging(unittest.TestCase):
+    def test_merge_unknown(self):
+        """Merge unknown types from multiple elements"""
+        class Numerical(object):
+            def __init__(self, value):
+                self.value = value
+
+            def __add__(self, other):
+                return self.value + other
+
+            def __radd__(self, other):
+                return self.value + other
+        inputs = [[1, 2, 3], [4, 5, 6], [7.5, 8.5, 9.5]]
+        inputs = [[Numerical(val) for val in chunk] for chunk in inputs]
+        chain = [produce(chunk) for chunk in inputs] >> chainlet.MergeLink()
+        with self.assertRaises(ValueError):
+            list(chain)
+        chain = [produce(chunk) for chunk in inputs] >> chainlet.MergeLink((Numerical, chainlet.dataflow.merge_numerical))
+        self.assertEqual(list(chain), [[sum(row)] for row in zip(*inputs)])
+
     def test_merge_numerical(self):
         """Merge numbers from multiple elements"""
         inputs = [[1, 2, 3], [4, 5, 6], [7.5, 8.5, 9.5]]
@@ -36,3 +55,13 @@ class ChainMerging(unittest.TestCase):
         ]
         chain = [produce(chunk) for chunk in inputs] >> chainlet.MergeLink()
         self.assertEqual(list(chain), [[{idx: str(idx), idx+3: str(idx+3)}] for idx in range(3)])
+
+    def test_merge_unbalanced(self):
+        """Merge from multiple elements with empty ones"""
+        for inputs in ([[1, 2, 3], [4, 5, 6], [7.5, 8.5, 9.5], []], [[1, 2, 3], [], []], [[], [], []]):
+            with self.subTest(inputs=inputs):
+                chain = [produce(chunk) for chunk in inputs] >> chainlet.MergeLink()
+                self.assertEqual(
+                    list(chain),
+                    [[sum(elem for elem in row if elem is not None)] for row in itertools.zip_longest(*inputs)]
+                )
