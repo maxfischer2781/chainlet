0b8e8970de89d37773c85b9601a922306e42d88b
Max Fischer <maxfischer2781@gmail.com>
2017-04-06 15:03:53 +0200

    StopTraversal is now exported at the namespace top

--- chainlet/__init__.py
+++ chainlet/__init__.py
@@ -1,7 +1,12 @@
 from __future__ import absolute_import
-from .chainlink import ChainLink
+from .chainlink import ChainLink, StopTraversal
 from .funclink import FunctionLink, funclet
 from .genlink import GeneratorLink, genlet
 from .dataflow import NoOp, joinlet, forklet, MergeLink
 
-__all__ = ['ChainLink', 'FunctionLink', 'funclet', 'GeneratorLink', 'genlet', 'NoOp', 'joinlet', 'forklet', 'MergeLink']
+__all__ = [
+    'ChainLink', 'StopTraversal',
+    'FunctionLink', 'funclet',
+    'GeneratorLink', 'genlet',
+    'NoOp', 'joinlet', 'forklet', 'MergeLink'
+]
