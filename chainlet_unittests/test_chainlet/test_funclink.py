f6844b69daa1e4dc8914d4d42d0fa704db9a09ba
Max Fischer <maxfischer2781@gmail.com>
2017-09-01 10:46:16 +0200

    extracted funlink subtests to separate tests

--- chainlet_unittests/test_chainlet/test_funclink.py
+++ chainlet_unittests/test_chainlet/test_funclink.py
@@ -1,36 +1,62 @@
 from __future__ import absolute_import, division
 import unittest
 import random
+try:
+    import cPickle as pickle
+except ImportError:
+    import pickle
 
 import chainlet
+import chainlet.funclink
 
 from chainlet_unittests.utility import NamedChainlet
 
 
-class FunctionLink(unittest.TestCase):
-    def test_linklet(self):
-        """Chainlink via decorator"""
-        @chainlet.funclet
-        def pingpong(value):
-            return value
+def pingpong(value):
+    return value
 
-        test_values = [0, 22, -22, 1E6, 'foobar'] + [random.random() for _ in range(20)]
-        with self.subTest(case='generator interface'):
-            funclet = pingpong()
+
+def new_pingpong():
+    return chainlet.funclink.FunctionLink(pingpong)
+
+@chainlet.funclet
+def pingponglet(value):
+    """Return a value unchanged"""
+    return value
+
+
+class TestFunctionLink(unittest.TestCase):
+    @staticmethod
+    def _get_test_iterable():
+        return [0, 22, -22, 1E6, 'foobar'] + [random.random() for _ in range(20)]
+
+    def test_funclet_generator(self):
+        """FunctionLink: generator interface"""
+        test_values = self._get_test_iterable()
+        for funclet in (pingponglet, new_pingpong):
+            funclet_instance = funclet()
             for value in test_values:
-                self.assertIsNone(next(funclet))
-                self.assertEqual(funclet.send(value), value)
-                self.assertIsNone(next(funclet))
-                self.assertEqual(funclet.send(value), value)
-                self.assertEqual(funclet.send(value), funclet.slave(value))  # funclet works like function
-        with self.subTest(case='chain element'):
-            chain = NamedChainlet('start') >> pingpong() >> NamedChainlet('stop')
+                self.assertIsNone(next(funclet_instance))
+                self.assertEqual(funclet_instance.send(value), value)
+                self.assertIsNone(next(funclet_instance))
+                self.assertEqual(funclet_instance.send(value), value)
+                self.assertEqual(funclet_instance.send(value), funclet_instance.slave(value))  # function preserved
+
+    def test_funclet_chain(self):
+        """FunctionLink: individual funclet in chain"""
+        test_values = self._get_test_iterable()
+        for funclet in (pingponglet, new_pingpong):
+            chain = NamedChainlet('start') >> funclet() >> NamedChainlet('stop')
             for value in test_values:
                 self.assertEqual(chain.send(value), value)
                 self.assertIsNone(next(chain))
                 self.assertEqual(chain.send(value), value)
-        with self.subTest(case='fill chain'):
-            chain = NamedChainlet('start') >> pingpong() >> pingpong() >> pingpong() >> pingpong() >> NamedChainlet('stop')
+
+    def test_long_chain(self):
+        """FunctionLink: chain of funclets"""
+        test_values = self._get_test_iterable()
+        for funclet in (pingponglet, new_pingpong):
+            chain = NamedChainlet('start') >> funclet() >> funclet() >> funclet() >> funclet() >> NamedChainlet('stop')
             for value in test_values:
                 self.assertEqual(chain.send(value), value)
                 self.assertIsNone(next(chain))
