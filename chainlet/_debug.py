b7d6e8bc6555f7545dc96854195851538dd26d77
Max Fischer <maxfischer2781@gmail.com>
2017-09-13 14:04:26 +0200

    fixed debug handler for bundles

--- chainlet/_debug.py
+++ chainlet/_debug.py
@@ -21,13 +21,13 @@ def chainlet_print_traceback(tb, file=None):
     traceback.print_tb(tb, limit=1, file=file)
     frame_name = tb.tb_frame.f_code.co_name
     frame_locals = dict(tb.tb_frame.f_locals)
-    if isinstance(frame_locals.get('self'), chainlet.ChainLink) and frame_name == 'chainlet_send':
+    if isinstance(frame_locals.get('self'), chainlet.ChainLink):
         self = frame_locals['self']
         cls = type(self)
         print('  for chainlet "%s:%s"' % (cls.__module__, cls.__name__), file=file, end='')
         try:
             print(', step %d state' % (self.elements.index(frame_locals['element'])), file=file)
-        except (IndexError, AttributeError):
+        except (IndexError, AttributeError, KeyError):
             print(', static state', file=file)
         for key in sorted(k for k in frame_locals if k != 'self'):
             print('    %s: %r' % (key, frame_locals[key]), file=file)
@@ -43,5 +43,5 @@ if __name__ == "__main__":
 
 
     noop = chainlet.dataflow.NoOp()
-    chn = noop >> noop >> fail() >> noop
+    chn = noop >> noop >> (fail(),) >> noop
     chn.send('initial value')
