81eab15b9d6ff76d932eed23d88c9c44f7ee5689
Max Fischer <maxfischer2781@gmail.com>
2017-09-13 13:53:03 +0200

    added unittests for decorating genlet and funclet

--- chainlet_unittests/test_dataflow/test_decorators.py
+++ chainlet_unittests/test_dataflow/test_decorators.py
@@ -11,6 +11,7 @@ from chainlet_unittests.utility import Adder, produce, abort_return, abort_swall
 
 class Decorators(unittest.TestCase):
     def test_chainlink(self):
+        """Decorate: class Join(ChainLink) ..."""
         @joinlet
         class Join(ChainLink):
             def chainlet_send(self, value=None):
@@ -22,6 +23,38 @@ class Decorators(unittest.TestCase):
                 return [value]
 
         self._test_flow(join_type=Join, fork_type=Fork)
+
+    def test_genlet(self):
+        """Decorate: @genlet ..."""
+        @joinlet
+        @genlet
+        def join():
+            value = yield
+            while True:
+                value = yield sum(value)
+
+        @forklet
+        @genlet
+        def fork():
+            value = yield
+            while True:
+                value = yield [value]
+
+        self._test_flow(join_type=join, fork_type=fork)
+
+    def test_funclet(self):
+        """Decorate: @funclet ..."""
+        @joinlet
+        @funclet
+        def join(value):
+            return sum(value)
+
+        @forklet
+        @funclet
+        def fork(value):
+            return [value]
+
+        self._test_flow(join_type=join, fork_type=fork)
 
     def _test_flow(self, join_type, fork_type):
         join_chain = (produce([1, 1, 1]), produce([2, 2, 2])) >> join_type()
