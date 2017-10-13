ac4d041735abfce92a76cf7119ca6ed4e64a350e
Max Fischer <maxfischer2781@gmail.com>
2017-10-13 16:29:53 +0200

    fixed ambiguity in driver tests

--- chainlet_unittests/test_chainlet/test_driver.py
+++ chainlet_unittests/test_chainlet/test_driver.py
@@ -11,28 +11,30 @@ from chainlet_unittests.utility import Adder, Buffer, produce
 
 class DriverMixin(object):
     driver_class = chainlet.driver.ChainDriver
+    buffer_class = Buffer
 
     def test_drive_single(self):
         """Drive a single chain"""
         driver = self.driver_class()
-        results = []
-        elements = [Adder(val) for val in (0, -2, 2, 1E6, random.randint(-256, 256), random.randint(-256, 256))]
-        for run_async in (True, False):
+        elements = [Adder(val) for val in (2, -2, 1E6, random.randint(-256, 256))]
+        for run_async in (False, True):
+            results = []
+            self.assertFalse(driver.mounts)
             with self.subTest(run_async=run_async):
                 for elements in itertools.product(elements, repeat=3):
                     initials = [0, 2, 1E6, -1232527]
                     expected = [initial + sum(element.value for element in elements) for initial in initials]
                     a, b, c = elements
-                    buffer = Buffer()
+                    buffer = self.buffer_class()
                     chain = produce(initials) >> a >> b >> c >> buffer
                     driver.mount(chain)
                     results.append([expected, buffer])
                 if run_async:
                     driver.start()
                     driver.start()  # starting multiple times is allowed
-                    time.sleep(0.1)  # let the driver startup
+                    time.sleep(0.05)  # let the driver startup
                     while driver.running:
-                        time.sleep(0.1)
+                        time.sleep(0.05)
                 else:
                     driver.run()
                 for expected, buffer in results:
