e9ee309aaa0a93f1348c70994bf915b2821c8d52
Max Fischer <maxfischer2781@gmail.com>
2018-01-23 16:38:38 +0100

    thread based concurrency uses futures

--- chainlet/concurrency/thread.py
+++ chainlet/concurrency/thread.py
@@ -3,37 +3,140 @@ Thread based concurrency domain
 """
 from __future__ import print_function
 import threading
+import os
+import time
+import atexit
+try:
+    import Queue as queue
+except ImportError:
+    import queue
 
 from .. import chainlink
+from .base import StoredFuture, canonical_send
 
 
-_THREAD_NOT_DONE = object()
+class ThreadPoolExecutor(object):
+    _min_workers = max(os.cpu_count(), 2)
+
+    def __init__(self, max_workers, identifier=''):
+        self._max_workers = max_workers
+        self._workers = set()
+        self._identifier = identifier or ('%s_%d' % (self.__class__.__name__, id(self)))
+        self._queue = queue.Queue()
+        self._ensure_worker()
+        atexit.register(self._teardown)
+
+    def _teardown(self):
+        while True:
+            try:
+                self._queue.get(block=False)
+            except queue.Empty:
+                break
+        for worker in range(len(self._workers)):
+            self._queue.put(None)
+        for worker in tuple(self._workers):
+            worker.join()
+
+    def submit(self, call, *args, **kwargs):
+        future = StoredFuture(call, *args, **kwargs)
+        self._queue.put(future)
+        self._ensure_worker()
+        return future
+
+    def execute_futures(self):
+        while True:
+            # try and get work
+            try:
+                future = self._queue.get(timeout=10)  # type: StoredFuture
+            except queue.Empty:
+                if self._dismiss_worker(threading.current_thread()):
+                    break
+            else:
+                if future is None:
+                    break
+                future.realise()
+                self._queue.task_done()
+        # clean up dangling threads
+        self._workers.discard(threading.current_thread())
+
+    def _dismiss_worker(self, worker):
+        """Dismiss ``worker`` unless it is still required"""
+        self._workers.remove(worker)
+        if len(self._workers) < self._min_workers:
+            self._workers.add(worker)
+            return False
+        return True
+
+    def _ensure_worker(self):
+        while len(self._workers) < self._min_workers or len(self._workers) < self._queue.qsize() < self._max_workers:
+            worker = threading.Thread(
+                target=self.execute_futures,
+                name=self._identifier + '_%d' % time.time(),
+            )
+            worker.daemon = True
+            self._workers.add(worker)
+            worker.start()
 
 
-class ThreadedCall(object):
-    def __init__(self, func, *args, **kwargs):
-        self._func = func
-        self._args = args
-        self._kwargs = kwargs
-        self._result = None
-        self._thread = threading.Thread(target=self)
-        self._thread.start()
+DEFAULT_EXECUTOR = ThreadPoolExecutor(os.cpu_count() * 5, 'chainlet_thread')
 
-    def __call__(self):
-        try:
-            self._result = self._func(*self._args, **self._kwargs), None
-        except Exception as err:
-            self._result = None, err
-        del self._func, self._args, self._kwargs
 
-    @property
-    def return_value(self):
-        if self._result is None:
-            self._thread.join()
-        result, error = self._result
-        if error:
-            raise error
-        return result
+class AsyncChainResults(object):
+    def __init__(self, futures):
+        self._futures = iter(futures)
+        self._results = []
+        self._exception = None
+        self._done = False
+        self._result_lock = threading.Lock()
+
+    def _set_done(self):
+        if not self._done:
+            self._done = True
+            self._futures = None
+            self._result_lock = None
+
+    def __iter__(self):
+        if self._done:
+            for item in self._results:
+                yield item
+        else:
+            for item in self._active_iter():
+                yield item
+        if self._exception is not None:
+            raise self._exception
+
+    def _active_iter(self):
+        result_idx = 0
+        # fast-forward existing results
+        for item in self._results:
+            yield item
+            result_idx += 1
+        # fetch remaining results safely
+        while self._futures and not self._exception:
+            # someone may have beaten us before we acquire this lock
+            # constraints must be rechecked as needed
+            with self._result_lock:
+                try:
+                    result = self._results[result_idx]
+                except IndexError:
+                    try:
+                        future = next(self._futures)
+                    except StopIteration:
+                        break
+                    try:
+                        results = future.results
+                    except Exception as err:
+                        self._exception = err
+                        break
+                    else:
+                        self._results.extend(results)
+                        for item in results:
+                            yield item
+                            result_idx += 1
+                else:
+                    yield result
+                    result_idx += 1
+        self._done = True
 
 
 class ThreadLinkPrimitives(chainlink.LinkPrimitives):
@@ -49,6 +152,7 @@ class ThreadBundle(chainlink.Bundle):
     to be run in parallel.
     """
     chain_types = ThreadLinkPrimitives()
+    executor = DEFAULT_EXECUTOR
 
     def _link_child(self, child):
         if child.chain_join:
@@ -69,8 +173,10 @@ class ThreadBundle(chainlink.Bundle):
         return self._link_child(self.chain_types.convert(child))
 
     def chainlet_send(self, value=None):
-        result_threads = self._dispath_send(value)
-        return self._collect_send(result_threads)
+        return AsyncChainResults([
+            self.executor.submit(canonical_send, element, [value])
+            for element in self.elements
+        ])
 
     def _dispath_send(self, value):
         result_threads = []
