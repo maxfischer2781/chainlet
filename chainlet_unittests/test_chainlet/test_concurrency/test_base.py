d41e295f9e63e1b58942899667ea138207f029ec
Max Fischer <maxfischer2781@gmail.com>
2018-01-28 09:58:35 +0100

    expanded test for multi-iter

--- chainlet_unittests/test_chainlet/test_concurrency/test_base.py
+++ chainlet_unittests/test_chainlet/test_concurrency/test_base.py
@@ -6,17 +6,20 @@ from chainlet.concurrency import base
 
 class TestMultiIter(unittest.TestCase):
     def test_builtin(self):
+        """multi iter on list, tuple, ..."""
         values = tuple(range(20))
         for base_type in (list, tuple, set):
             iterable = base_type(values)
             self._test_multi_tee(iterable, values)
 
     def test_generator(self):
+        """multi iter on `(val for val in values)`"""
         values = tuple(range(20))
         iterable = (val for val in values)
         self._test_multi_tee(iterable, values)
 
     def test_future_chain(self):
+        """multi iter on future chain results"""
         values = tuple(range(20))
         value_iter = iter(values)
         iterable = base.FutureChainResults([base.StoredFuture(lambda itr: [next(itr)], value_iter) for _ in range(len(values))])
@@ -29,8 +32,13 @@ class TestMultiIter(unittest.TestCase):
         # test single iteration
         self.assertEqual(set(a), set(values))
         # test interleaved iteration
-        for _ in range(10):
+        for _ in range(9):
             self.assertEqual(next(b), next(c))
             self.assertEqual(next(c), next(b))
+        self.assertEqual((next(b), next(b)), (next(c), next(c)))
+        with self.assertRaises(StopIteration):
+            next(b)
+        with self.assertRaises(StopIteration):
+            next(c)
         # test final iteration
         self.assertEqual(set(d), set(values))
