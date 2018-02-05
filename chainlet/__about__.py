45053fb1956a75dd827b1b3bd34bd4a469cd9e63
Max Fischer <maxfischer2781@gmail.com>
2018-02-05 15:28:27 +0100

    version bump

--- chainlet/__about__.py
+++ chainlet/__about__.py
@@ -104,7 +104,7 @@ __title__ = 'chainlet'
 __summary__ = 'Framework for linking generators/iterators to processing chains, trees and graphs'
 __url__ = 'https://github.com/maxfischer2781/chainlet'
 
-__version__ = '1.3.0'
+__version__ = '1.3.1'
 __author__ = 'Max Fischer'
 __email__ = 'maxfischer2781@gmail.com'
 __copyright__ = '2016 - 2018 %s' % __author__
